b5db4a6952b10452b21cea48240880b1f597937c
Shabtay Shalem <shabtay.shalem@asml.com>
2026-03-08 00:29:42 +0200

    03-08-2026  0:29

--- vrss-frontend/src/pages/Illumination/IlluminationComponents/IL1_TempPkg/TempILMain.tsx
+++ vrss-frontend/src/pages/Illumination/IlluminationComponents/IL1_TempPkg/TempILMain.tsx
@@ -1,15 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-
-// ===========================================
-// ==     content menu (title name)         ==
-// ===========================================
-
-// ===========================================
-// == Update anchorList with  content menu  ==
-// ===========================================
-
-// ============================================
-// ============================================
+import { Loading } from "../../../../components";
 
 const TempSWMain = () => {
   const [isLoading, setIsLoading] = useState(true);
@@ -32,14 +22,56 @@ const TempSWMain = () => {
     return () => clearTimeout(timer);
   }, []);
 
-  // setTimeout(() => {
-  //   setIsLoading(false);
-  // }, 200);
-
   if (isLoading) {
     return <Loading />;
   }
 
-  return <section className="ml-2 md:ml-10">test</section>;
+  return (
+    <section className="bg-gray-200">
+      <div>
+        <article className="flex h-28 items-center justify-between gap-1">
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+          <div className="flex h-[6.5rem] w-full flex-col justify-center border-2 border-gray-300 pl-1">
+            <p>8600</p>
+            <p className="text-sm text-gray-400">machine number</p>
+          </div>
+        </article>
+
+        <div className="my-1"></div>
+        <main className="h-4/5 border-2 border-gray-300">main</main>
+      </div>
+    </section>
+  );
 };
 export default TempSWMain;
